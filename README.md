d1046f6cbe6b265ec3133e28082c0311b539deda
Jake Ryan <jwr32@psu.edu>
2021-12-03 15:46:42 -0500

    Mockup pcbox

--- README.md
+++ README.md
@@ -2,12 +2,28 @@
 -   Jacob Ryan _jwr32_
 -   Luke McCleery _lfm5334_
 
-## Data Model
-7.  Trainers
-8.  Parties
-1.  Pokemon
-2.  PokemonSpecies
-3.  Types
-4.  Moves
-5.  Type Match-Ups
-6.  Abilities
+## To-Do
+Preloaded Data:
+-   Types
+-   Pokemon Types
+-   Pokemon Species
+-   Moves
+-   Type Matchups
+
+Data Creatable through Interface
+-   Trainers
+-   Trainers' Pokemon
+-   Party Pokemon
+-   Pokemon Moves
+
+Interfaces Needed
+-   Create Trainer
+-   PC Box
+	-   Add Pokemon To Party
+	-   Remove from Party
+	-   Rename Pokemon
+	-   Release Pokemon
+	-   View Party
+-   Pokemon Status
+-   Pokedex
+-   Catch Pokemon
---
+++ js/pc_box.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $(".card").on("click", function (event) {
+        $('li[data-action=add]').show();
+        $('li[data-action=remove]').hide();
+        $(".context-menu").finish().toggle(100).css({
+            left: event.pageX + "px",
+            top: event.pageY + "px"
+        });
+    });
+    $(".slot").on("click", function (event) {
+        $('li[data-action=remove]').show();
+        $('li[data-action=add]').hide();
+        $(".context-menu").finish().toggle(100).css({
+            left: event.pageX + "px",
+            top: event.pageY + "px"
+        });
+    });
+
+    $(".context-menu li").click(function () {
+        $(".context-menu").hide(100);
+    });
+
+
+});
+$(document).bind("mousedown", function (e) {
+
+    // If the clicked element is not the menu
+    if ($(e.target).parents(".context-menu").length <= 0) {
+        $(".context-menu").hide(100);
+    }
+});
